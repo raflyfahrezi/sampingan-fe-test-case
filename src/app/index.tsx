eec813f2f02a8a08629124ad2fe8a91bce4e3487
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-27 19:05:27 +0700

    fix app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import { Home } from '@/modules'
 import { PageWrapper } from '@/layout'
 
 const App = () => {
     return (
         <PageWrapper>
-            <div>App</div>
+            <Home />
         </PageWrapper>
     )
 }
