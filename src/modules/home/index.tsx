978bd03c8fda1a7311be32d6ea6c7210d06ae3d3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-27 19:04:57 +0700

    fix home modules

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Wrapper } from '@/components'
+
+const Home = () => {
+    return (
+        <Wrapper>
+            <div>Home</div>
+        </Wrapper>
+    )
+}
+
+export default Home
