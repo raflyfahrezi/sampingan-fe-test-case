087ebb7991e2a9e5e09cf81004486a394e18fcd9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-27 22:51:43 +0700

    fix pagewrapper

--- src/layout/pageWrapper/index.tsx
+++ src/layout/pageWrapper/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-import { Head, Header } from '@/layout'
+import { Head, Header, Footer } from '@/layout'
 
+import { sPageWrapper } from './styles'
 import { PageWrapperProps } from './types'
 
 const PageWrapper = ({ children }: PageWrapperProps) => {
     return (
         <>
             <Head />
-            <div>
-                <Header />
-                {children}
+            <div className={sPageWrapper}>
+                <div>
+                    <Header />
+                    {children}
+                </div>
+                <Footer />
             </div>
         </>
     )
---
+++ src/layout/pageWrapper/styles.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/css'
+
+export const sPageWrapper = css`
+    width: 100%;
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: 1fr auto;
+`
